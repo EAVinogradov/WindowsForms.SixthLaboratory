e350a01d6173982abdf7eb201492e59295f416c5
EAVinogradov <e.a.vinogradov@icloud.com>
2020-06-03 16:47:39 +0300

    Добавьте файлы проекта.

---
+++ EAVinogradov.CSWindowsForms.SixthLaboratory.FirstExample/EditForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAVinogradov.CSWindowsForms.SixthLaboratory.FirstExample
+{
+    public partial class EditForm : Form
+    {
+        public EditForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            WinGrafSinLine frmOne = this.Owner as WinGrafSinLine;
+                try
+                {
+                    frmOne.a = double.Parse(textBox1.Text);
+                    frmOne.b = double.Parse(textBox2.Text);
+                }
+                catch (Exception er)
+                {
+                    MessageBox.Show("При выполнении ввода исходных данных возникла ошибка: \n" + er.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                this.Close();
+                frmOne.Refresh();
+        }
+
+    }
+}
